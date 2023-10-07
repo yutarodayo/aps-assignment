3a09ddb01d354eca8cc678fe680f7e252c9e43fa
yutarodayo <yutarotweet@gmail.com>
2023-10-07 20:51:21 +0900

    10/7

---
+++ src/js/script.js
@@ -0,0 +1,119 @@
+
+jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+
+// drawer
+$(".js-hamburger").on("click", function (e) {
+    e.preventDefault();
+    $(".js-hamburger").toggleClass("is-active");
+    $(".header").toggleClass("is-active");
+    $(".sp-nav__wrapper").toggleClass("is-active");
+    return false;
+  });
+
+  // /drawer
+
+  //swiper
+  const swiper = new Swiper(".swiper", {
+    // 前後の矢印
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    
+    loop: true,
+
+    // スライドの表示枚数：768px未満の場合
+  slidesPerView: 1.2,
+  spaceBetween: 20,
+  breakpoints: {
+    450: {
+      slidesPerView: 1.5,
+    },
+
+    // スライドの表示枚数：768px以上の場合
+    600: {
+      slidesPerView: 2,
+    },
+    
+    750: {
+      slidesPerView: 2.5,
+      spaceBetween: 40,
+    //スライド間の余白を指定（px）。
+    // CSSで margin を付けるとスライドの位置がずれていくことがあるため、こちらで指定するのが推奨。
+    },
+    
+    1250: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+
+    1440: {
+      slidesPerView: 3.4,
+      spaceBetween: 40,
+    },
+
+  },
+
+  });
+
+  // /swiper
+
+
+  /*===================================================
+  to top + スクロール検知
+  ===================================================*/
+// $(window).on("scroll", function() {
+//   // トップから100px以上スクロールしたら
+//   if (100 < $(this).scrollTop()) {
+//     // is-showクラスをつける
+//  $('.to_top').addClass( 'is_show' );
+//   } else {
+//     // 100pxを下回ったらis-showクラスを削除
+//   $('.to_top').removeClass( 'is_show' );
+//   }
+// });
+// ページトップボタン
+$(function () {
+  const to_top = $("#to_top");
+  to_top.hide();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      to_top.fadeIn();
+    } else {
+      to_top.fadeOut();
+    }
+  });
+  to_top.click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      500
+    );
+    return false;
+  });
+  // フッター手前でストップ
+  $(window).on("scroll", function () {
+    let scrollHeight = $(document).height();
+    let scrollPosition = $(window).height() + $(window).scrollTop();
+    let footHeight = $("footer").innerHeight();
+    if (scrollHeight - scrollPosition <= footHeight) {
+ // ページトップボタンがフッター手前に来たらpositionとfixedからabsoluteに変更
+      $("#to_top").css({
+        position: "absolute",
+        bottom: footHeight + 15,
+      });
+    } else {
+      $("#to_top").css({
+        position: "fixed",
+        bottom: "15px",
+      });
+    }
+  });
+});
+
+/*===================================================
+/to top + スクロール検知
+===================================================*/
+
+});
