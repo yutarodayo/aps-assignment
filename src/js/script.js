1124cc1cc53b9091659dfdae6bd5d3e96ef91c37
yutarodayo <yutarotweet@gmail.com>
2023-10-08 07:35:16 +0900

    10/8レスポンシブ前

--- src/js/script.js
+++ src/js/script.js
@@ -116,4 +116,89 @@ $(function () {
 /to top + スクロール検知
 ===================================================*/
 
+/*===================================================
+背景色⇒表示
+===================================================*/
+//要素の取得とスピードの設定
+var box = $('.colorbox'),
+    speed = 700;  
+ 
+//.colorboxの付いた全ての要素に対して下記の処理を行う
+box.each(function(){
+    $(this).append('<div class="color"></div>')
+    var color = $(this).find($('.color')),
+    image = $(this).find('img');
+    var counter = 0;
+ 
+    image.css('opacity','0');
+    color.css('width','0%');
+    //inviewを使って背景色が画面に現れたら処理をする
+    color.on('inview', function(){
+        if(counter == 0){
+　　　　　$(this).delay(200).animate({'width':'100%'},speed,function(){
+                   image.css('opacity','1');
+                   $(this).css({'left':'0' , 'right':'auto'});
+                   $(this).animate({'width':'0%'},speed);
+                })
+            counter = 1;
+          }
+     });
+});
+
+var box = $('.colorbox_right'),
+    speed = 700;  
+ 
+//.colorboxの付いた全ての要素に対して下記の処理を行う
+box.each(function(){
+    $(this).append('<div class="color"></div>')
+    var color = $(this).find($('.color')),
+    image = $(this).find('img');
+    var counter = 0;
+ 
+    image.css('opacity','0');
+    color.css('width','0%');
+    //inviewを使って背景色が画面に現れたら処理をする
+    color.on('inview', function(){
+        if(counter == 0){
+　　　　　$(this).delay(200).animate({'width':'100%'},speed,function(){
+                   image.css('opacity','1');
+                   $(this).css({'right':'0' , 'left':'auto'});
+                   $(this).animate({'width':'0%'},speed);
+                })
+            counter = 1;
+          }
+     });
+});
+
+
+var box = $('.colorbox_top'),
+    speed = 700;  
+ 
+//.colorboxの付いた全ての要素に対して下記の処理を行う
+box.each(function(){
+    $(this).append('<div class="color"></div>')
+    var color = $(this).find($('.color')),
+    image = $(this).find('img');
+    var counter = 0;
+ 
+    image.css('opacity','0');
+    color.css('height','0%');
+    //inviewを使って背景色が画面に現れたら処理をする
+    color.on('inview', function(){
+        if(counter == 0){
+　　　　　$(this).delay(200).animate({'height':'100%'},speed,function(){
+                   image.css('opacity','1');
+                   $(this).css({'bottom':'0' , 'top':'auto'});
+                   $(this).animate({'height':'0%'},speed);
+                })
+            counter = 1;
+          }
+     });
+});
+
+
+/*===================================================
+/背景色⇒表示
+===================================================*/
+
 });
